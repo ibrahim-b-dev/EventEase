aed6a6fcb108dd858d976fad2c98353ecf51da46
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-11-20 14:34:12 +0300

    create hello world endpoint

---
+++ backend/utils/logger.js
@@ -0,0 +1,13 @@
+const info = (...params) => {
+  console.info(...params)
+}
+
+const warn = (...params) => {
+  console.warn(...params)
+}
+
+const error = (...params) => {
+  console.error(...params)
+}
+
+module.exports = { info, warn, error }
