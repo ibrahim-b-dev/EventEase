76108d22d016e76b1fb7c300c6f821d05b72d426
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-11-20 23:15:20 +0300

    Design the schema for the RSVPs collection

---
+++ backend/models/rsvp.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// A schema defines the structure of collection documents.
+const rsvpSchema = new mongoose.Schema({
+  RSVP_ID: { type: String, required: true, unique: true, index: true },
+  userID: { type: String, required: true, ref: 'User', index: true }, // Foreign key reference
+  eventID: { type: String, required: true, ref: 'Event', index: true }, // Foreign key reference
+  RSVP_Status: { 
+    type: String, 
+    required: true, 
+    enum: ['Yes', 'No', 'Maybe'], 
+    default: 'Maybe' 
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+
+// Models take schema and apply it to each document in its collection.
+// Models are responsible for all document interactions (CRUD).
+const RSVP = mongoose.model("RSVP", rsvpSchema)
+
+module.exports = RSVP
