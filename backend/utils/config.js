83165d0f2a3cdef4c8f940b46544945e13541be5
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-11-21 11:34:56 +0300

    add local mongodb uri constant

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
+// console.log("Loaded Environment Variables:", process.env)
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const LOCAL_MONGODB_URI = process.env.LOCAL_MONGODB_URI
 
 module.exports = {
   PORT,
   MONGODB_URI,
+  LOCAL_MONGODB_URI,
 }
