fd9d0b0fb4f22f5c12d03f89c40e156dfa2db250
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-11-24 22:52:24 +0300

    Register controller require phone

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,7 @@ const register = async (req, res) => {
 
   if (password.length < 6) {
     return res.status(400).json({
-      error: "Password must be at least 6 charactters long",
+      error: "Password must be at least 6 characters long",
     })
   }
 
