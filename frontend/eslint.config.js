acfa851abc6acca367d5ee844f2e787a2a7352d2
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-11-26 20:34:04 +0300

    chore: Configure frontend tools

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,38 +1,67 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import babelParser from "@babel/eslint-parser"
 
 export default [
-  { ignores: ['dist'] },
+  // Base configuration from @eslint/js
+  js.configs.recommended,
+
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.js", "**/*.jsx"],
+    ignores: ["dist", "eslint.config.js"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: babelParser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react",
+          ],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        document: "readonly",
+        window: "readonly",
       },
     },
-    settings: { react: { version: '18.3' } },
+
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    settings: {
+      react: {
+        version: "18.2",
+      },
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "arrow-spacing": ["error", { before: true, after: true }],
+      "no-console": 0,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": 0,
+      "no-unused-vars": 0,
     },
   },
 ]
