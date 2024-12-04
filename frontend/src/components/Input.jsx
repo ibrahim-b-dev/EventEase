4db73bd019920f1f0ece36aae8bdab27ecf059a7
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-12-04 18:20:04 +0300

    style: update input component style

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,12 +1,15 @@
 import styled from "styled-components"
 
 const InputContainer = styled.div`
-  border: 1px solid #a0a1a5;
+  border-width: 1px;
+  border-style: solid;
   border-radius: 5px;
+  border-color: ${({ bordercolor }) => (bordercolor ? bordercolor : "#a0a1a5")};
   display: flex;
   align-items: center;
   padding: 0.5rem 1rem;
-  color: #a0a1a5;
+  color: ${({ color }) => (color ? color : "#a0a1a5")};
+  background-color: ${({ bgcolor }) => bgcolor || "transparent"};
 `
 
 const StyledInput = styled.input`
@@ -16,11 +19,15 @@ const StyledInput = styled.input`
   font-size: 1rem;
   color: inherit;
   margin-left: 0.5rem;
+
+  &::placeholder {
+    color: ${(props) => props.placeholdercolor || "#a0a1a5"};
+  }
 `
 
 const Input = ({ type = "text", placeholder, Icon, onChange, ...props }) => {
   return (
-    <InputContainer>
+    <InputContainer {...props}>
       {Icon && <Icon />}
       <StyledInput
         type={type}
