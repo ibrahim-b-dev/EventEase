21a0067dd5faec7fefb0024dad84b46b3210572f
ibrahim-b-dev <ibrahim.dev.b@gmail.com>
2024-12-09 15:08:56 +0300

    refactor: update loginService to use client for better reusability

--- frontend/src/services/loginService.js
+++ frontend/src/services/loginService.js
@@ -1,15 +1,12 @@
-import axios from "axios"
-const baseUrl = "/api/"
+import client from "./client"
 
 const login = async (credentials) => {
-  const loginUrl = `${baseUrl}login`
-  const request = await axios.post(loginUrl, credentials)
+  const request = await client.post("/auth/login", credentials)
   return request.data
 }
 
 const register = async (credentials) => {
-  const registerUrl = `${baseUrl}login`
-  const request = await axios.post(registerUrl, credentials)
+  const request = await client.post("/auth/register", credentials)
   return request.data
 }
 
